5e6c924d8becb7ec1f3cb2af14555bb98b62a0e8
laiyurui <45766023+laiyurui@users.noreply.github.com>
2024-06-06 21:40:37 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,10 @@
 # photo
-# done
 python main.py --model GCN --dataset photo  --cfg ./cfg/gcn.yaml --gpu 0  
 python main.py --model GCN2 --dataset photo --cfg ./cfg/gcn2.yaml --gpu 0
 python main.py --model EbdGNN --dataset photo   --cfg ./cfg/TADA_gcn.yaml  --se_type 'rwr' --se_dim 256 --gamma 0.3 --rho 0.3 --beta 1.0  --gpu 0 
 python main.py --model EbdGNN --dataset photo --cfg ./cfg/TADA_gcn2.yaml  --se_type 'rwr' --se_dim 256  --gamma 0.3 --rho 0.3 --beta 1.0  --gpu 0
 
 # squirrel
-# done 
 python main.py --model GCN --dataset squirrel  --cfg ./cfg/gcn.yaml --gpu 0
 python main.py --model GCN2 --dataset squirrel --cfg ./cfg/gcn2.yaml --gpu 0
 python main.py --model EbdGNN --dataset squirrel  --cfg ./cfg/TADA_gcn.yaml  --se_type 'rwr' --se_dim 128 --gamma 1.0 --rho 0.5 --beta 1.0  --gpu 0
